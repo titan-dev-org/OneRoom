f6f802e394071f518c6bae54c91dcff6141d7f8d
titan-dev-org <titanthree31@gmail.com>
2026-09-02 16:32:03 +0700

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,11 +1,11 @@
 // lib/supabase/server.ts
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient as createSupabaseServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createServerClient = () => {
   const cookieStore = cookies()
   
-  return createServerClient(
+  return createSupabaseServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
